947244c215a13e51fffe6c2af831e74db9c68bf8
Adrià Gomis <gomisroca@gmail.com>
2025-05-15 14:27:17 +0200

    feat: word creation form

--- frontend/src/app/components/words/create/create.component.ts
+++ frontend/src/app/components/words/create/create.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { WordsService } from '@app/services/words.service';
 
 @Component({
   selector: 'words-create',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create.component.html',
 })
-export class WordsCreateComponent {}
+export class WordsCreateComponent {
+  word = '';
+  type = '';
+  gender = '';
+  plural = '';
+  level = '';
+  definition = '';
+  examples = '';
+
+  private wordsService = inject(WordsService);
+
+  onSubmit() {
+    this.wordsService
+      .create({
+        word: this.word,
+        type: this.type,
+        gender: this.gender,
+        plural: this.plural,
+        level: this.level,
+        definition: this.definition,
+        examples: this.examples.split('/'),
+      })
+      .subscribe((word) => {
+        console.log('Created word:', word);
+      });
+  }
+}
