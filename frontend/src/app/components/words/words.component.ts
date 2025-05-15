d22f8b946871080bf99f4a8317ce0a50c4378191
Adrià Gomis <gomisroca@gmail.com>
2025-05-15 13:07:24 +0200

    feat: add link to word creation form

--- frontend/src/app/components/words/words.component.ts
+++ frontend/src/app/components/words/words.component.ts
@@ -3,12 +3,12 @@ import { WordsService } from '@services/words.service';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { Word } from 'types';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 import { WordsCardComponent } from './card/card.component';
+import { LinkComponent } from '../ui/link/link.component';
 
 @Component({
   selector: 'words',
-  imports: [AsyncPipe, RouterLink, RouterLinkActive, WordsCardComponent],
+  imports: [AsyncPipe, WordsCardComponent, LinkComponent],
   templateUrl: './words.component.html',
 })
 export class WordsComponent {
