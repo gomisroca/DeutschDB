6e40347e33a6f2848b6cbc9cd4488a388a7fb33a
Adrià Gomis <gomisroca@gmail.com>
2025-05-15 16:25:52 +0200

    fix: organise update logic

--- frontend/src/app/components/words/update/update.component.ts
+++ frontend/src/app/components/words/update/update.component.ts
@@ -2,57 +2,59 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { WordsService } from '@app/services/words.service';
+import { Word } from 'types';
 
 @Component({
   selector: 'words-update',
+  standalone: true,
   imports: [FormsModule],
   templateUrl: './update.component.html',
 })
 export class WordsUpdateComponent {
+  form: {
+    id: string;
+    word: string;
+    type: string;
+    gender: string;
+    plural: string;
+    level: string;
+    definition: string;
+    examples?: string;
+  } = {
+    id: '',
+    word: '',
+    type: '',
+    gender: '',
+    plural: '',
+    level: '',
+    definition: '',
+    examples: '',
+  };
+
   constructor(
     private wordsService: WordsService,
     private route: ActivatedRoute
   ) {}
 
-  word = '';
-  type = '';
-  gender = '';
-  plural = '';
-  level = '';
-  definition = '';
-  examples = '';
-
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    if (id)
-      this.wordsService
-        .getUnique(id)
-        .subscribe(
-          (word) => (
-            (this.word = word.word),
-            (this.type = word.type),
-            (this.gender = word.gender),
-            (this.plural = word.plural),
-            (this.level = word.level),
-            (this.definition = word.definition),
-            (this.examples = word.examples?.join('/') ?? '')
-          )
-        );
+    if (!id) return;
+
+    this.form.id = id;
+
+    this.wordsService.getUnique(id).subscribe((word) => {
+      this.form = {
+        ...word,
+        examples: word.examples.join(',') ?? '',
+      };
+    });
   }
 
   onSubmit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-
     this.wordsService
       .update({
-        id: id as string,
-        word: this.word,
-        type: this.type,
-        gender: this.gender,
-        plural: this.plural,
-        level: this.level,
-        definition: this.definition,
-        examples: this.examples.split('/'),
+        ...this.form,
+        examples: this.form.examples?.split(',') ?? [],
       })
       .subscribe((word) => {
         console.log('Updated word:', word);
