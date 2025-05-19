817686504e02c9e1757bd11442424dca7599bce4
Adrià Gomis <gomisroca@gmail.com>
2025-05-19 09:22:46 +0200

    fix: main.ts envs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,19 +4,17 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
-  const configService = app.get(ConfigService);
   app.enableCors({
-    origin: configService.get('FRONTEND_URL'),
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:4200',
     methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
-  await app.listen((configService.get('PORT') as number) ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
