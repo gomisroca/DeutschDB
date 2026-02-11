47eb59dcadeb1e9639cf8c9f83b091e725a87ff4
Adrià Gomis <gomisroca@gmail.com>
2026-02-11 11:27:34 +0100

    update verbs frontend service to use api service base

--- frontend/src/app/services/verbs.service.ts
+++ frontend/src/app/services/verbs.service.ts
@@ -1,42 +1,10 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from '@environments/environment';
+import { Injectable } from '@angular/core';
 import { VerbConjugation, Verb } from 'types';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class VerbsService {
-  private http = inject(HttpClient);
-  private baseUrl = `${environment.API_URL}/verbs`;
-
-  getById(id: string): Observable<VerbConjugation> {
-    return this.http.get<VerbConjugation>(`${this.baseUrl}/${id}`);
-  }
-
-  getAll(params?: {
-    skip?: number;
-    take?: number;
-    cursor?: string;
-  }): Observable<Verb[]> {
-    return this.http.get<Verb[]>(this.baseUrl, { params });
-  }
-
-  create(
-    data: Omit<VerbConjugation, 'id'> & { verbName: string },
-  ): Observable<Verb> {
-    return this.http.post<Verb>(this.baseUrl, data);
-  }
-
-  update(
-    id: string,
-    data: Partial<Omit<VerbConjugation, 'id'>>,
-  ): Observable<Verb> {
-    return this.http.patch<Verb>(`${this.baseUrl}/${id}`, data);
-  }
-
-  delete(id: string): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/${id}`);
-  }
+export class VerbsService extends ApiService<Verb, VerbConjugation> {
+  protected endpoint = 'verbs';
 }
