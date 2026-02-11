7ea4dfe96e52c0c1ff52135f8d63dbbcc47ebfcc
Adrià Gomis <gomisroca@gmail.com>
2026-02-11 11:27:28 +0100

    update words frontend service to use api service base

--- frontend/src/app/services/words.service.ts
+++ frontend/src/app/services/words.service.ts
@@ -1,39 +1,16 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from '@environments/environment';
+import { Injectable } from '@angular/core';
 import { Word } from 'types';
+import { ApiService } from './api.service';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class WordsService {
-  private http = inject(HttpClient);
-  private baseUrl = `${environment.API_URL}/words`;
+interface WordsQuery {
+  type?: string;
+  level?: string;
+  skip?: number;
+  take?: number;
+  cursor?: string;
+}
 
-  getById(id: string): Observable<Word> {
-    return this.http.get<Word>(`${this.baseUrl}/${id}`);
-  }
-
-  getAll(params?: {
-    type?: string;
-    level?: string;
-    skip?: number;
-    take?: number;
-    cursor?: string;
-  }): Observable<Word[]> {
-    return this.http.get<Word[]>(this.baseUrl, { params });
-  }
-
-  create(data: Omit<Word, 'id'>): Observable<Word> {
-    return this.http.post<Word>(this.baseUrl, data);
-  }
-
-  update(id: string, data: Partial<Omit<Word, 'id'>>): Observable<Word> {
-    return this.http.patch<Word>(`${this.baseUrl}/${id}`, data);
-  }
-
-  delete(id: string): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/${id}`);
-  }
+@Injectable({ providedIn: 'root' })
+export class WordsService extends ApiService<Word, WordsQuery> {
+  protected endpoint = 'words';
 }
