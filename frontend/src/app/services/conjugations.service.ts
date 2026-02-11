ce4e9acc687dd0f2fce6b56be4319cbd7895e9f2
Adrià Gomis <gomisroca@gmail.com>
2026-02-11 11:33:22 +0100

    fix passed queries

--- frontend/src/app/services/conjugations.service.ts
+++ frontend/src/app/services/conjugations.service.ts
@@ -1,42 +1,26 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { VerbConjugation } from 'types';
+import { ApiService } from './api.service';
 import { Observable } from 'rxjs';
-import { environment } from '@environments/environment';
-import { VerbConjugation, Verb } from 'types';
+
+interface ConjugationQuery {
+  skip?: number;
+  take?: number;
+  cursor?: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class ConjugationsService {
-  private http = inject(HttpClient);
-  private baseUrl = `${environment.API_URL}/conjugations`;
+export class ConjugationsService extends ApiService<
+  VerbConjugation,
+  ConjugationQuery
+> {
+  protected endpoint = 'conjugations';
 
-  getById(id: string): Observable<VerbConjugation> {
-    return this.http.get<VerbConjugation>(`${this.baseUrl}/${id}`);
-  }
-
-  getAll(params?: {
-    skip?: number;
-    take?: number;
-    cursor?: string;
-  }): Observable<VerbConjugation[]> {
-    return this.http.get<VerbConjugation[]>(this.baseUrl, { params });
-  }
-
-  create(
+  override create(
     data: Omit<VerbConjugation, 'id'> & { verbName: string },
   ): Observable<VerbConjugation> {
     return this.http.post<VerbConjugation>(this.baseUrl, data);
-  }
-
-  update(
-    id: string,
-    data: Partial<Omit<VerbConjugation, 'id'>>,
-  ): Observable<VerbConjugation> {
-    return this.http.patch<VerbConjugation>(`${this.baseUrl}/${id}`, data);
-  }
-
-  delete(id: string): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/${id}`);
   }
 }
--- frontend/src/app/services/verbs.service.ts
+++ frontend/src/app/services/verbs.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
-import { VerbConjugation, Verb } from 'types';
+import { Verb } from 'types';
 import { ApiService } from './api.service';
+
+interface VerbsQuery {
+  skip?: number;
+  take?: number;
+  cursor?: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class VerbsService extends ApiService<Verb, VerbConjugation> {
+export class VerbsService extends ApiService<Verb, VerbsQuery> {
   protected endpoint = 'verbs';
 }
